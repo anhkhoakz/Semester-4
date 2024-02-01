c761caf83ec7d633ea9f9db08971befe923c3f81
anhkhoakz <given_amber066@silomails.com>
2024-02-01 12:24:33 +0700

    Add files

---
+++ Introduction_to_Digital_Image_Processing/Labs/Lab01/ex01-Additional.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+imagePath = "imgs/fruits.png"
+image=cv2.imread(imagePath)
+
+m, n, _ = image.shape
+
+avgI = 0
+
+for r in range ( m):
+    for c in range ( n):
+        avgI+= image[r, c].mean()
+
+avgI /= (m+n)
+
+print(avgI)
+
+# cv2.imshow('Image', image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
